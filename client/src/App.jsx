d117cb9e411a31558c2dba73cb7293ad357389ca
MAHESH VUNNAM <MY_NAME@example.com>
2025-05-11 23:14:35 +0530

    initial setup for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className='text-4xl'>job tracker</p>
+    </>
+  )
+}
+
+export default App
